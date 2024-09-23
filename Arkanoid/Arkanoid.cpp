6b62a491387767dcd3b8b958f082f3cfa57c2a2a
ximemolina <ximenamolinaportilla01@gmail.com>
2024-09-23 17:18:20 -0600

    Create Arkanoid.cpp

---
+++ Arkanoid/Arkanoid.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//			SE CREA MENÚ DE JUEGO Y SE INICIA EL JUEGO PRINCIPAL
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <time.h>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
