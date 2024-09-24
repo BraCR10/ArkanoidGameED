260d40e30b32bd2acb10714aae12f6e2ceba7b2a
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-09-23 18:21:50 -0600

    Configuracion basica

--- Arkanoid/Arkanoid.cpp
+++ Arkanoid/Arkanoid.cpp
@@ -1,5 +1,6 @@
+
+// Librerias de C++
 #include <iostream>
-//			SE CREA MENÚ DE JUEGO Y SE INICIA EL JUEGO PRINCIPAL
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
@@ -8,6 +9,7 @@
 #include <cstdlib>
 #include <time.h>
 
+//Librerias de Allegro
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
@@ -17,17 +19,11 @@
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
 using namespace std;
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
